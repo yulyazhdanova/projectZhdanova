8cb16ca1f1050d27dd68b65dca416452bc7ed73e
[yulya_zhdanova] <[yulya_zhdanova_1992@mail.ru]>
2022-06-19 13:09:52 +0300

    правки к коментариям 6 ДЗ

--- src/test/java/zhdanova/tests/RegistrationFormWithFakerTest.java
+++ src/test/java/zhdanova/tests/RegistrationFormWithFakerTest.java
@@ -1,53 +1,42 @@
 package zhdanova.tests;
 
 import org.junit.jupiter.api.Test;
-
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static zhdanova.tests.TestData.*;
+import zhdanova.pages.RegistrationFormPage;
 
 public class RegistrationFormWithFakerTest extends TestBase {
     
     @Test
     void successfulTest() {
 
+        RegistrationFormPage registrationFormPage = new RegistrationFormPage();
         TestData testData = new TestData();
 
         registrationFormPage.openPage()
-                .setFirstName(TestData.firstName)
-                .setLastName(TestData.lastName)
-                .setEmail(TestData.userEmail)
-                .setGender(TestData.gender)
-                .setNumber(TestData.userNumber)
-                .setDateOfBirth(TestData.yearOfBirth,TestData.monthOfBirth, TestData.dateOfBirth)
-                .setSubjects(TestData.subject)
-                .setHobbies(TestData.hobbie)
-                .setPicture()
-                .setAddress(TestData.currentAddress)
-                .setState(USERSTATE)
-                .setCity(USERCITY)
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setEmail(testData.userEmail)
+                .setGender(testData.gender)
+                .setNumber(testData.userNumber)
+                .setDateOfBirth(testData.yearOfBirth,testData.monthOfBirth, testData.dateOfBirth)
+                .setSubjects(testData.subject)
+                .setHobbies(testData.hobby)
+                .setPicture(testData.PICTURE)
+                .setAddress(testData.currentAddress)
+                .setState(testData.USERSTATE)
+                .setCity(testData.USERCITY)
                 .submit()
-                .checkText()
-                .checkText2();
+                .checkSummaryOfForm()
+                .checkSummaryOfFinalForm();
 
-        registrationFormPage.checkResult("Student Name", TestData.firstName + " " + TestData.lastName)
-                .checkResult("Student Email", TestData.userEmail)
-                .checkResult("Gender", TestData.gender)
-                .checkResult("Mobile", TestData.userNumber)
-                .checkResult("Date of Birth", dateOfBirth + " " + TestData.monthOfBirth + "," + yearOfBirth)
-                .checkResult("Subjects", TestData.subject)
-                .checkResult("Hobbies", TestData.hobbie)
-                .checkResult("Picture", PICTURE)
-                .checkResult("Address", TestData.currentAddress)
-                .checkResult("State and City", USERSTATE + " " + USERCITY);
+        registrationFormPage.checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                .checkResult("Student Email", testData.userEmail)
+                .checkResult("Gender", testData.gender)
+                .checkResult("Mobile", testData.userNumber)
+                .checkResult("Date of Birth", testData.dateOfBirth + " " + testData.monthOfBirth + "," + testData.yearOfBirth)
+                .checkResult("Subjects", testData.subject)
+                .checkResult("Hobbies", testData.hobby)
+                .checkResult("Picture", testData.PICTURE)
+                .checkResult("Address", testData.currentAddress)
+                .checkResult("State and City", testData.USERSTATE + " " + testData.USERCITY);
     }
-
-    void checkTable(String key, String value) {
-        $(".table-responsive").$(byText(key))
-                .parent().shouldHave(text(value));
-    }
-
-
 }
--- src/test/java/zhdanova/tests/TestData.java
+++ src/test/java/zhdanova/tests/TestData.java
@@ -10,19 +10,19 @@ public class TestData {
 
     static Faker faker = new Faker();
 
-    static String firstName = faker.name().firstName();
-    static String lastName = faker.name().lastName();
-    static String userEmail = faker.internet().emailAddress();
-    static String gender = RandomsForForm.randomGender();
-    static String userNumber = getRandomNumber(10);
-    static String dateOfBirth = String.valueOf(faker.number().numberBetween(1,25));
-    static String monthOfBirth = RandomsForForm.randomMonth();
-    static String yearOfBirth = String.valueOf(faker.number().numberBetween(1990,2000));
-    static String subject = RandomsForForm.randomSubjects();
-    static String hobbie = RandomsForForm.randomHobbie();
-    public static  final String PICTURE = "Screenshot.png";
-    static String currentAddress = faker.address().fullAddress();
-    public final static String USERSTATE = "NCR";
-    public final static String USERCITY = "Gurgaon";
+    final String firstName = faker.name().firstName();
+    final String lastName = faker.name().lastName();
+    final String userEmail = faker.internet().emailAddress();
+    final String gender = RandomsForForm.randomGender();
+    final String userNumber = getRandomNumber(10);
+    final String dateOfBirth = String.valueOf(faker.number().numberBetween(1,25));
+    final String monthOfBirth = RandomsForForm.randomMonth();
+    final String yearOfBirth = String.valueOf(faker.number().numberBetween(1990,2000));
+    final String subject = RandomsForForm.randomSubjects();
+    final String hobby = RandomsForForm.randomHobbie();
+    final String PICTURE = "img/Screenshot.png";
+    final String currentAddress = faker.address().fullAddress();
+    final String USERSTATE = "NCR";
+    final String USERCITY = "Gurgaon";
 
 }
